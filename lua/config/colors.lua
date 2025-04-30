d958cc25685efad3b3319d4d3aba750b9c4ddc35
CUKENGER <andreychuvashov2015@gmail.com>
2025-05-01 00:10:35 +0300

    fix: transparent

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -2,10 +2,10 @@ function SetColor(color)
 	color = color or "onedark_vivid"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "#120E27" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#0E0A23" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "#120E27" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#0E0A23" })
 	vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
 	vim.api.nvim_set_hl(0, "LineNr", { fg = "#6B7280", bg = "none" })
 end
 
-SetColor("onedark_vivid")
+SetColor("onedark")
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,79 +1,80 @@
 return {
- {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/cmp-buffer'},
-  {'hrsh7th/cmp-path'},
-  {'hrsh7th/cmp-cmdline'},
-  {'hrsh7th/nvim-cmp'},
+	{ 'neovim/nvim-lspconfig' },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/cmp-buffer' },
+	{ 'hrsh7th/cmp-path' },
 
-  -- Для vsnip пользователей
-  {'hrsh7th/cmp-vsnip'},
-  {'hrsh7th/vim-vsnip'},
+	{ 'hrsh7th/cmp-cmdline' },
+	{ 'hrsh7th/nvim-cmp' },
 
-  -- Настройка nvim-cmp
-  {
-    'hrsh7th/nvim-cmp',
-		  dependencies = {
-      "windwp/nvim-autopairs",
-      opts = {},
-    },
-    config = function()
-      local cmp = require'cmp'
+	-- Для vsnip пользователей
+	{ 'hrsh7th/cmp-vsnip' },
+	{ 'hrsh7th/vim-vsnip' },
 
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- Для пользователей vsnip
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-					['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i', 's'}),
-          ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i', 's'}),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' }, -- Для пользователей vsnip
-        }, {
-          { name = 'buffer' },
-        }),
+	-- Настройка nvim-cmp
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			"windwp/nvim-autopairs",
+			opts = {},
+		},
+		config = function()
+			local cmp = require 'cmp'
+
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body) -- Для пользователей vsnip
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 's' }),
+					['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 's' }),
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'vsnip' }, -- Для пользователей vsnip
+				}, {
+					{ name = 'buffer' },
+				}),
 				-- formatting = {
 				--       before = require("tailwind-tools.cmp").lspkind_format
 				--     },
-      })
+			})
 
-      -- Настройка для командной строки
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
+			-- Настройка для командной строки
+			cmp.setup.cmdline({ '/', '?' }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = 'buffer' }
+				}
+			})
 
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-      })
+			cmp.setup.cmdline(':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = 'path' }
+				}, {
+					{ name = 'cmdline' }
+				})
+			})
 
-      -- Настройка lspconfig
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      require('lspconfig')['emmet_ls'].setup {
-        capabilities = capabilities
-      }
-    end,
-  },
+			-- Настройка lspconfig
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			require('lspconfig')['emmet_ls'].setup {
+				capabilities = capabilities
+			}
+		end,
+	},
 }
